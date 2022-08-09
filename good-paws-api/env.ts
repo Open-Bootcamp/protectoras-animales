c9316e8c293e87dbd9e60040f211afcf1cd8b623
Ezequiel-Manfredi <ezemanfre@gmail.com>
2022-08-09 20:28:35 -0300

    Database configuration

--- good-paws-api/env.ts
+++ good-paws-api/env.ts
@@ -21,4 +21,9 @@ export default Env.rules({
   APP_NAME: Env.schema.string(),
   DRIVE_DISK: Env.schema.enum(['local'] as const),
   NODE_ENV: Env.schema.enum(['development', 'production', 'test'] as const),
+  PG_HOST: Env.schema.string({ format: 'host' }),
+  PG_PORT: Env.schema.number(),
+  PG_USER: Env.schema.string(),
+  PG_PASSWORD: Env.schema.string.optional(),
+  PG_DB_NAME: Env.schema.string(),
 })
