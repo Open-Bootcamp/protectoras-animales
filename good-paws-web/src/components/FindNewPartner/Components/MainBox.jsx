49160bcf8d12d49aaeabd3ac3c8a59f96cfb822d
Roqueheli <roqueheliarenas@gmail.com>
2022-08-18 12:45:03 -0400

    removing paddin on mainbox

--- good-paws-web/src/components/FindNewPartner/Components/MainBox.jsx
+++ good-paws-web/src/components/FindNewPartner/Components/MainBox.jsx
@@ -5,7 +5,7 @@ import Card from "./Card";
 
 const MainBox = ({ data }) => {
   return (
-    <Grid h={'100vh'} p={4} w="full" gridGap="1" gridTemplateColumns="repeat( auto-fit, minmax(245px, 1fr) )">
+    <Grid h={'100vh'} w="full" gridGap="1" gridTemplateColumns="repeat( auto-fit, minmax(245px, 1fr) )">
       {data.map((p) => (
         <Card key={p.id} {...p} />
       ))}
