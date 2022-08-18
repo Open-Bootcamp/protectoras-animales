92bed40c3fbf238662d2ba35f28a3f0f9baa5c64
Ezequiel-Manfredi <ezemanfre@gmail.com>
2022-08-18 19:33:48 -0300

    fix: rule config error

--- good-paws-api/app/Validators/Schemas/CenterSchema.ts
+++ good-paws-api/app/Validators/Schemas/CenterSchema.ts
@@ -5,7 +5,7 @@ export const centerSchema = schema.create({
   name: schema.string(),
   location: schema.string(),
   coordinates: schema.string({}, [rules.regex(coordinatesRegex)]),
-  protectorId: schema.number([rules.exists({ table: 'centers', column: 'protector_id' })]),
+  protectorId: schema.number([rules.exists({ table: 'protectors', column: 'id' })]),
   picture: schema.string({}, [rules.regex(imagesRegex)]),
   status: schema.boolean.optional(),
 })
