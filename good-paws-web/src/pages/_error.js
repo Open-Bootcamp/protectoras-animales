f90207ff9e9729f27119264dff2458b4fa77e46a
Roqueheli <roqueheliarenas@gmail.com>
2022-08-22 16:56:18 -0400

    refactoring custom errors

--- good-paws-web/src/pages/_error.js
+++ good-paws-web/src/pages/_error.js
@@ -8,14 +8,18 @@ function Error({ statusCode }) {
 
     return (
         <Flex h={'100vh'} direction={'column'} justifyContent={'center'} alignItems={'center'}>
-            {customError && <Text lineHeight={'24px'} size={'16px'} fontWeight={600} color={colors.primarylight}>{customError.code} error</Text>}
-            <Heading mb={3} textAlign={'center'} maxW={'100%'} fontSize={{ base: '3xl', md: '4xl', lg: '6xl' }}>
-                <Text>{customError.title}</Text>
-            </Heading>
-            <Text size={'20px'} p={6} fontWeight={400} lineHeight={'30px'} mb={6} color={colors.error}>{customError.description}</Text>
-            <Link href="/">
-                <Button color={colors.white} _active={{ bg: colors.primarypastel }} _hover={{ bg: colors.primarylight }} bg={colors.primary}>Volver al inicio</Button>
-            </Link>
+            {customError && 
+                <>
+                    <Text lineHeight={'24px'} size={'16px'} fontWeight={600} color={colors.primarylight}>{customError.code ? customError.code : '404'} error</Text>
+                    <Heading mb={3} textAlign={'center'} maxW={'100%'} fontSize={{ base: '3xl', md: '4xl', lg: '6xl' }}>
+                        <Text>{customError.title ? customError.title : '¡Página no encontrada!'}</Text>
+                    </Heading>
+                    <Text size={'20px'} p={6} fontWeight={400} lineHeight={'30px'} mb={6} color={colors.error}>{customError.description ? customError.description : 'Lo siento, no hemos encontrado lo que estabas buscando.'}</Text>
+                    <Link href="/">
+                        <Button color={colors.white} _active={{ bg: colors.primarypastel }} _hover={{ bg: colors.primarylight }} bg={colors.primary}>Volver al inicio</Button>
+                    </Link>
+                </>
+            }
         </Flex>
     )
 }
