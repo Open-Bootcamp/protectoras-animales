b57998b6d39bfd51c760036b8ddb291cb8941ff3
Ezequiel-Manfredi <ezemanfre@gmail.com>
2022-08-24 09:07:12 -0300

    feat: add routes fro reset password

--- good-paws-api/start/routes/auth.ts
+++ good-paws-api/start/routes/auth.ts
@@ -2,3 +2,5 @@ import Route from '@ioc:Adonis/Core/Route'
 
 Route.post('/login', 'AuthenticationsController.login')
 Route.post('/logout', 'AuthenticationsController.logout').middleware('auth')
+Route.post('/forgot', 'AuthenticationsController.forgot')
+Route.post('/reset', 'AuthenticationsController.reset').middleware('auth')
