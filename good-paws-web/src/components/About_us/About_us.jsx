d3d001aa89736b61e7eb8224ac9b7b804af6fccb
diloes <diloes@hotmail.com>
2022-08-26 00:55:52 +0200

    fix: styles in about_us component

--- good-paws-web/src/components/About_us/About_us.jsx
+++ good-paws-web/src/components/About_us/About_us.jsx
@@ -1,19 +1,25 @@
-import React from "react";
-import styled from "styled-components";
-import { FeatureIcon, Subtitle, Title } from "../ui";
+import React from 'react'
+import styled from 'styled-components'
+import { FeatureIcon, Subtitle, Title } from '../ui'
 
 export const Container = styled.div`
   margin: 0px 12px;
   background-color: #f9fafb;
   text-align: center;
+  padding: 6rem 0;
+
+  @media (prefers-color-scheme: dark) {
+    color: black;
+  }
 
   @media (max-width: 600px) {
     padding: 0px;
     height: auto;
     margin: 0px 12px;
-    text-align: left;
+    text-align: center;
+    padding: 1rem;
   } ;
-`;
+`
 export const Wrapper = styled.div`
   display: inline-flex;
   padding: 0px 100px;
@@ -26,31 +32,31 @@ export const Wrapper = styled.div`
     flex-direction: column;
     align-items: center;
   } ;
-`;
+`
 const SubtitleWraper = styled.div`
   @media (min-width: 600px) {
     padding: 10px 25%;
   }
-`;
+`
 const Features = styled.div`
   margin-top: 40px;
   text-align: center;
   padding: 0px 13px;
-`;
+`
 
 export const About_us = () => {
   return (
     <Container>
-      <Title mx="0px" weight="600">
+      <Title mx="0px" mb="1.5rem" weight="600" size="36px">
         ¿Qué sómos?
       </Title>
       <SubtitleWraper>
         <Subtitle weight="500" size="20px" height="30px">
-          Somos un proyecto Open Source, destinado a ayudar a las protectoras
-          para que puedan realizar su trabajo más cómodamente.
+          Somos un proyecto Open Source, destinado a ayudar a las protectoras para que puedan
+          realizar su trabajo más cómodamente.
         </Subtitle>
       </SubtitleWraper>
-      <div style={{ display: "flex" }}>
+      <div style={{ display: 'flex' }}>
         <Wrapper>
           <Features>
             <FeatureIcon icon="users" />
@@ -58,8 +64,7 @@ export const About_us = () => {
               Open Source
             </Title>
             <Subtitle weigth="400" size="16px" height="24px" align="center">
-              Somos un proyecto desarrollado por OpenDevs, sin ánimo de lucro y
-              100% gratuito.
+              Somos un proyecto desarrollado por OpenDevs, sin ánimo de lucro y 100% gratuito.
             </Subtitle>
           </Features>
 
@@ -69,8 +74,7 @@ export const About_us = () => {
               Visibilidad
             </Title>
             <Subtitle weigth="400" size="16px" height="24px">
-              Ayudamos a que todos los animales encuentren un hogar donde ser
-              felices.
+              Ayudamos a que todos los animales encuentren un hogar donde ser felices.
             </Subtitle>
           </Features>
 
@@ -80,12 +84,11 @@ export const About_us = () => {
               Especializado
             </Title>
             <Subtitle weigth="400" size="16px" height="24px">
-              Le damos a las protectoras las herramientas que necesitan para
-              llevar a cabo su labor.
+              Le damos a las protectoras las herramientas que necesitan para llevar a cabo su labor.
             </Subtitle>
           </Features>
         </Wrapper>
       </div>
     </Container>
-  );
-};
+  )
+}
