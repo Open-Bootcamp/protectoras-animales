4057671c57b51fbfc3f6f0dc4ed33cb9d17fb991
Grecia Alva Bermejo <gz.alvabermejo@gmail.com>
2022-08-25 11:42:43 +0200

    Added Fonts from Chakra to customize font.

--- good-paws-web/src/pages/_app.js
+++ good-paws-web/src/pages/_app.js
@@ -2,6 +2,7 @@ import { ChakraProvider } from "@chakra-ui/react";
 import Layout from "../components/Layout/Layout";
 import "../styles/globals.css";
 import theme from "../styles/ChakraTheme";
+import "@fontsource/inter";
 
 function MyApp({ Component, pageProps }) {
   return (
--- good-paws-web/src/styles/ChakraTheme.jsx
+++ good-paws-web/src/styles/ChakraTheme.jsx
@@ -1,6 +1,10 @@
 import { extendTheme } from "@chakra-ui/react";
 
 const theme = extendTheme({
+  fonts: {
+    heading: `"Inter", sans-serif`,
+    body: `"Inter", sans-serif`,
+  },
   colors: {
     gray5: "#3c3f41",
     gray4: "#697172",
