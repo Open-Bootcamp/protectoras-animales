74794e980ae2dc30fd757030685998c1993a2d1f
Roqueheli <roqueheliarenas@gmail.com>
2022-08-28 16:15:39 -0400

    adding button to go back home page

--- good-paws-web/src/components/CardsContainer/CardsContainer.jsx
+++ good-paws-web/src/components/CardsContainer/CardsContainer.jsx
@@ -1,5 +1,6 @@
 import React, { useContext } from "react";
-import { Grid, Flex, Heading } from "@chakra-ui/react";
+import NextLink from 'next/link';
+import { Button, Grid, Flex, Heading } from "@chakra-ui/react";
 import Card from "../Card/Card";
 import MoonLoader from 'react-spinners/MoonLoader';
 import { MainContext } from "../../context/maincontext";
@@ -10,7 +11,13 @@ const CardsContainer = () => {
   return (
     <Flex mb={10} alignItems={'center'} justifyContent={'center'} h={'full'}>
       { isLoading ? <MoonLoader size={100} color="#578887" /> :
-        data.length === 0 ? <Heading>No existe data para mostrar</Heading> :
+        data.length === 0 ? 
+        <Flex direction={'column'} alignItems={'center'}>
+          <Heading mb={5}>No existe data para mostrar</Heading>
+          <NextLink href="/">
+              <Button color={'white'} _active={{ bg: 'primarypastel' }} _hover={{ bg: 'primarylight' }} bg={'primary'}>Volver al inicio</Button>
+          </NextLink>
+        </Flex> :
           <Grid h={'full'} w="full" gridGap="1" gridTemplateColumns="repeat( auto-fit, minmax(245px, 1fr) )">
             {data.map((p) => ( <Card key={p.id} {...p} /> ))}
           </Grid>
