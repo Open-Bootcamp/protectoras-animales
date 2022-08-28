1773b9db1ec55f8a9f2530ff513556d9d7025919
Roqueheli <roqueheliarenas@gmail.com>
2022-08-28 16:05:46 -0400

    adding loader spinner and validations newpartner

--- good-paws-web/src/components/CardsContainer/CardsContainer.jsx
+++ good-paws-web/src/components/CardsContainer/CardsContainer.jsx
@@ -1,16 +1,20 @@
-import React from "react";
-import { Grid, Flex } from "@chakra-ui/react";
+import React, { useContext } from "react";
+import { Grid, Flex, Heading } from "@chakra-ui/react";
 import Card from "../Card/Card";
+import MoonLoader from 'react-spinners/MoonLoader';
+import { MainContext } from "../../context/maincontext";
 
 
-const CardsContainer = ({ data }) => {
+const CardsContainer = () => {
+  const { data, isLoading } = useContext(MainContext);
   return (
     <Flex mb={10} alignItems={'center'} justifyContent={'center'} h={'full'}>
-      <Grid h={'full'} w="full" gridGap="1" gridTemplateColumns="repeat( auto-fit, minmax(245px, 1fr) )">
-        {data.map((p) => (
-          <Card key={p.id} {...p} />
-        ))}
-      </Grid>
+      { isLoading ? <MoonLoader size={100} color="#578887" /> :
+        data.length === 0 ? <Heading>No existe data para mostrar</Heading> :
+          <Grid h={'full'} w="full" gridGap="1" gridTemplateColumns="repeat( auto-fit, minmax(245px, 1fr) )">
+            {data.map((p) => ( <Card key={p.id} {...p} /> ))}
+          </Grid>
+      }
     </Flex>
   );
 };
