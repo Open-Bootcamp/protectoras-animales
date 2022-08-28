68fb2cd890582b11c7a45080335410c08ec60ba2
KussFrSo <marcfranch.fh@gmail.com>
2022-08-28 16:46:20 +0200

    feat: message pictures

---
+++ good-paws-api/app/Models/MessageImage.ts
@@ -0,0 +1,20 @@
+import { DateTime } from 'luxon'
+import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
+import { attachment, AttachmentContract } from '@ioc:Adonis/Addons/AttachmentLite'
+
+export default class MessageImage extends BaseModel {
+  @column({ isPrimary: true })
+  public id: number
+
+  @column({ serializeAs: 'messagelId' })
+  public messageId: number
+
+  @attachment({ folder: 'message', preComputeUrl: true })
+  public picture: AttachmentContract
+
+  @column.dateTime({ autoCreate: true, serializeAs: null })
+  public createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true, serializeAs: null })
+  public updatedAt: DateTime
+}
--- good-paws-api/start/routes/message.ts
+++ good-paws-api/start/routes/message.ts
@@ -3,6 +3,16 @@ import Route from '@ioc:Adonis/Core/Route'
 Route.group(() => {
   Route.get('/', 'MessagesController.index')
   Route.post('/', 'MessagesController.store')
+  Route.group(() => {
+    Route.get('/', 'MessageImagesController.index')
+    Route.post('/', 'MessageImagesController.store')
+    Route.group(() => {
+      Route.get('/', 'MessageImagesController.show')
+      Route.delete('/', 'MessageImagesController.destroy')
+    })
+      .prefix('/:idImage')
+      .where('id', Route.matchers.number())
+  }).prefix('/images')
 })
   .prefix('/messages')
   .middleware('auth')
