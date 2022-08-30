ac8da5ad650cbf7b5241b9fb68a84b53ddd55068
Ezequiel-Manfredi <ezemanfre@gmail.com>
2022-08-30 16:37:26 -0300

    feat: add review model and migration

---
+++ good-paws-api/app/Models/Review.ts
@@ -0,0 +1,31 @@
+import { DateTime } from 'luxon'
+import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
+
+export default class Review extends BaseModel {
+  @column({ isPrimary: true })
+  public id: number
+
+  @column()
+  public title: string
+
+  @column()
+  public text: string
+
+  @column()
+  public rate: number
+
+  @column()
+  public authorId: number
+
+  @column()
+  public reviewedId: number
+
+  @column()
+  public status: boolean
+
+  @column.dateTime({ autoCreate: true, serialize: (value) => value.toFormat('yyyy-LL-dd') })
+  public createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true, serializeAs: null })
+  public updatedAt: DateTime
+}
