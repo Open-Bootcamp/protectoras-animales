164793d1a89912b82328171112db72ab355d2861
Roqueheli <roqueheliarenas@gmail.com>
2022-09-02 14:05:54 -0400

    filters finished now yes

--- good-paws-web/src/components/FindNewPartner/components/HeadBox.jsx
+++ good-paws-web/src/components/FindNewPartner/components/HeadBox.jsx
@@ -3,7 +3,6 @@ import { Heading, Text, Stack, Tabs, TabList, Tab } from "@chakra-ui/react";
 import { MainContext } from '../../../context/maincontext';
 
 const HeadBox = () => {
-  const [tabIndex, setTabIndex] = React.useState(-1);   // assumes at least one of the options is active
   const { filters, handleChange } = useContext(MainContext);
   const [isElder, setIsElder] = useState(false);
   const [canTravel, setCanTravel] = useState(false);
@@ -11,29 +10,26 @@ const HeadBox = () => {
   
   const handleIsElder = (e) => {
     setIsElder(!isElder);
-    e.target.value = isElder;
+    e.target.value = !isElder;
     e.target.name = e.target.name;
     handleChange(e);
   }
 
   const handleHasEspecialCondition = (e) => {
+    e.preventDefault();
     setHasEspecialCondition(!hasEspecialCondition);
-    e.target.value = hasEspecialCondition;
+    e.target.value = !hasEspecialCondition;
     e.target.name = e.target.name;
     handleChange(e);
   }
 
   const handleCanTravel = (e) => {
+    e.preventDefault();
     setCanTravel(!canTravel);
-    e.target.value = canTravel;
+    e.target.value = !canTravel;
     e.target.name = e.target.name;
     handleChange(e);
   }
-
-  const handleTabsChange = (index) => {
-    setTabIndex(index);
-  }
-
 
   return (
     <Stack mb={5} minH={"auto"} direction={{ base: "column", md: "column" }}>
@@ -43,11 +39,11 @@ const HeadBox = () => {
         </Text>
       </Heading>
       <Stack direction={{ base: "column", md: "row" }} spacing={4}>
-        <Tabs index={tabIndex} onChange={handleTabsChange} variant="unstyled" colorScheme={'primary'}>
+        <Tabs variant="unstyled" colorScheme={'primary'}>
           <TabList>
-            <Tab name={'isElder'} value={filters.isElder} onClick={handleIsElder} m={2} rounded={'md'} border='1px solid' bg={isElder ? 'none' : 'primary'} color={isElder ? 'black' : 'white'}>Adopta un senior</Tab>
-            <Tab name={'hasEspecialCondition'} value={filters.hasEspecialCondition} onClick={handleHasEspecialCondition} m={2} rounded={'md'} border='1px solid' bg={hasEspecialCondition ? 'none' : 'primary'} color={hasEspecialCondition ? 'black' : 'white'}>Condición especial</Tab>
-            <Tab name={'canTravel'} m={2} value={filters.canTravel} onClick={handleCanTravel} rounded={'md'} border='1px solid' bg={canTravel ? 'none' : 'primary'} color={canTravel ? 'black' : 'white'}>Puede ir de viaje</Tab>
+            <Tab name={'isElder'} value={Boolean(filters.isElder)} onClick={handleIsElder} m={2} rounded={'md'} border='1px solid' bg={Boolean(isElder) === false ? 'none' : 'primary'} color={Boolean(isElder) === false ? 'black' : 'white'}>Adopta un senior</Tab>
+            <Tab name={'hasEspecialCondition'} value={Boolean(filters.hasEspecialCondition)} onClick={handleHasEspecialCondition} m={2} rounded={'md'} border='1px solid' bg={Boolean(hasEspecialCondition) === false ? 'none' : 'primary'} color={Boolean(hasEspecialCondition) === false ? 'black' : 'white'}>Condición especial</Tab>
+            <Tab name={'canTravel'} m={2} value={Boolean(filters.canTravel)} onClick={handleCanTravel} rounded={'md'} border='1px solid' bg={Boolean(canTravel) === false ? 'none' : 'primary'} color={Boolean(canTravel) === false ? 'black' : 'white'}>Puede ir de viaje</Tab>
           </TabList>
         </Tabs>
       </Stack>
