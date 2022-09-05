ec0555c5aeb36a2d5b40eaf176ba6a5662672096
Cesk <ceskk97@gmail.com>
2022-09-05 21:44:39 +0200

    Botfix: fixing errors and warnings  and refactoring

--- good-paws-web/src/pages/_document.js
+++ good-paws-web/src/pages/_document.js
@@ -35,7 +35,11 @@ class MyDocument extends Document {
       <Html>
         <Head>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+          <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin="true"
+          />
           <link
             href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&display=swap"
             rel="stylesheet"
