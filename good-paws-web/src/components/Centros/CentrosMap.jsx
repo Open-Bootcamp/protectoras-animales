46da731aa86b80c717392654ff8d91563d8c363a
Cesk <ceskk97@gmail.com>
2022-09-06 12:54:25 +0200

    feat: Centro components refactored

--- good-paws-web/src/components/Centros/CentrosMap.jsx
+++ good-paws-web/src/components/Centros/CentrosMap.jsx
@@ -1,58 +1,55 @@
-import React, {useEffect, useState} from "react"
-import styles from '../../styles/Centros/CentrosMap.module.css'
-import 'leaflet/dist/leaflet.css'
-import { MapContainer, TileLayer, Marker, Popup, useMap } from 'react-leaflet'
-import CentroCard from "./CentroCard"
+import React, { useEffect } from "react";
+import styles from "../../styles/Centros/CentrosMap.module.css";
+import "leaflet/dist/leaflet.css";
+import { MapContainer, TileLayer, Marker, Popup, useMap } from "react-leaflet";
+import CentroCard from "./CentroCard";
 
-const CentrosMap = ({centros}) => {
+const CentrosMap = ({ centros, userLocation }) => {
+  const mapOptions = {
+    center: [userLocation[0], userLocation[1]],
+    zoom: 6,
+    markerIcon: new L.Icon({ iconUrl: "/marker.svg" }),
+    markerIconUser: new L.icon({ iconUrl: "/markerGreen.svg" }),
+  };
 
-    const mapOptions = {
-        // Centro marcado en Madrid, con un zoom que abarque toda España
-        'center': centros.length == 1
-            ? [centros[0].coordinates.split(", ")[0], centros[0].coordinates.split(", ")[1]]
-            : [40.4165, -3.70256],
-        'zoom': centros.length == 1
-            ? 14
-            : 6,
-        'markerIcon': new L.Icon({ iconUrl: '/marker.svg'})
-    }
+  function CurrentLocation() {
+    const map = useMap();
+    useEffect(() => {
+      map.locate().on("locationfound", (e) => {
+        map.flyTo(
+          {
+            lat: centros[0].coordinates.split(",")[0],
+            lng: centros[0].coordinates.split(",")[1],
+          },
+          7
+        );
+      });
+    }, []);
+  }
 
-    function CurrentLocation() {
-        const map = useMap()
-        useEffect(() => {
-            map.locate().on("locationfound", (e) => {
-                map.flyTo(e.latlng, 8)
-            })
-        }, [])
-    }
+  return (
+    <div className={styles.centrosMap}>
+      <MapContainer center={mapOptions.center} zoom={mapOptions.zoom}>
+        <TileLayer url="https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png" />
+        {centros.length != 0 &&
+          Object.keys(centros).map((key) => (
+            <Marker
+              key={key}
+              position={[
+                centros[key].coordinates.split(",")[0],
+                centros[key].coordinates.split(",")[1],
+              ]}
+              icon={mapOptions.markerIcon}
+            >
+              <Popup minWidth={260}>
+                <CentroCard centro={centros[key]} isMap={true} />
+              </Popup>
+            </Marker>
+          ))}
+        {centros.length == 1 && <CurrentLocation />}
+      </MapContainer>
+    </div>
+  );
+};
 
-    return (
-        <div className={styles.centrosMap}>
-            <MapContainer center={mapOptions.center} zoom={mapOptions.zoom}>
-				<TileLayer
-					url='https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png'
-				/>
-                { centros.length != 0
-                    ? Object.keys(centros).map((key) => 
-                    <Marker
-                        key={key}
-                        position={[
-                            centros[key].coordinates.split(", ")[0], 
-                            centros[key].coordinates.split(", ")[1]
-                        ]}
-                        icon={mapOptions.markerIcon}
-                    >
-                        <Popup minWidth={260}>
-                            <CentroCard centro={centros[key]} isMap={true} />
-                        </Popup>
-                    </Marker>
-                    )
-                    : null
-                }
-                {centros.length != 1 ? <CurrentLocation /> : null}
-			</MapContainer>
-        </div>
-    )
-}
-
-export default CentrosMap
+export default CentrosMap;
