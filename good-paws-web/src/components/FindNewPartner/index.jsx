69ba63af79b9ff5e98d5f0f0fd80f99791a1fb49
Roqueheli <roqueheliarenas@gmail.com>
2022-09-08 15:43:41 -0400

    refactoring shelters map

--- good-paws-web/src/components/FindNewPartner/index.jsx
+++ good-paws-web/src/components/FindNewPartner/index.jsx
@@ -1,15 +1,6 @@
 import React, { useContext, useEffect } from "react";
 import { useClickable } from "@chakra-ui/clickable";
-import {
-  chakra,
-  Text,
-  Box,
-  Divider,
-  Container,
-  Flex,
-  Stack,
-  useColorModeValue,
-} from "@chakra-ui/react";
+import { chakra, Text, Box, Divider, Container, Flex, Stack, useColorModeValue } from "@chakra-ui/react";
 import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
 import HeadBox from "./Components/HeadBox";
 import CardsContainer from "../CardsContainer/CardsContainer";
@@ -22,20 +13,7 @@ import { usePagination } from "react-use-pagination";
 const Clickable = (props) => {
   const clickable = useClickable(props);
   return (
-    <chakra.button
-      border={`1px solid gray`}
-      rounded={"md"}
-      w={8}
-      h={8}
-      cursor={"pointer"}
-      onClick
-      display={"inline-flex"}
-      alignItems={"center"}
-      justifyContent={"center"}
-      transition={"background 0.3s ease"}
-      _hover={{ bg: "primary", color: "white" }}
-      {...clickable}
-    />
+    <chakra.button border={`1px solid gray`} rounded={"md"} w={8} h={8} cursor={"pointer"} onClick display={"inline-flex"} alignItems={"center"} justifyContent={"center"} transition={"background 0.3s ease"} _hover={{ bg: "primary", color: "white" }} {...clickable} />
   );
 };
 
@@ -59,42 +37,11 @@ const FindNewPartner = () => {
             <CardsContainer data={data.animals.results && data.animals.results} />
             <Box color={useColorModeValue("gray5", "gray2")}>
               <Divider orientation="horizontal" />
-              <Container
-                as={Stack}
-                maxW={"6xl"}
-                py={4}
-                direction={{ base: "column", md: "row" }}
-                spacing={4}
-                justify={{ base: "center", md: "space-between" }}
-                align={{ base: "center", md: "center" }}
-              >
-                <Flex
-                  w={"100%"}
-                  justifyContent={"space-between"}
-                  direction={"row"}
-                  spacing={6}
-                  alignItems={"center"}
-                >
-                  <Clickable
-                    as="button"
-                    onClick={setPreviousPage}
-                    _active={{ bg: "primarylight", color: "white" }}
-                    _disabled={!previousEnabled}
-                  >
-                    <FaArrowLeft />
-                  </Clickable>
-                  <Text>
-                    Página {totalPages > 0 ? currentPage + 1 : currentPage} de{" "}
-                    {totalPages}
-                  </Text>
-                  <Clickable
-                    as="button"
-                    onClick={setNextPage}
-                    _active={{ bg: "primarylight", color: "white" }}
-                    _disabled={!nextEnabled}
-                  >
-                    <FaArrowRight />
-                  </Clickable>
+              <Container as={Stack} maxW={"6xl"} py={4} direction={{ base: "column", md: "row" }} spacing={4} justify={{ base: "center", md: "space-between" }} align={{ base: "center", md: "center" }}>
+                <Flex w={"100%"} justifyContent={"space-between"} direction={"row"} spacing={6} alignItems={"center"}>
+                  <Clickable as="button" onClick={setPreviousPage} _active={{ bg: "primarylight", color: "white" }} _disabled={!previousEnabled}><FaArrowLeft /></Clickable>
+                  <Text>Página {totalPages > 0 ? currentPage + 1 : currentPage} de{" "}{totalPages}</Text>
+                  <Clickable as="button" onClick={setNextPage} _active={{ bg: "primarylight", color: "white" }} _disabled={!nextEnabled}><FaArrowRight /></Clickable>
                 </Flex>
               </Container>
             </Box>
--- good-paws-web/src/components/Shelters/index.jsx
+++ good-paws-web/src/components/Shelters/index.jsx
@@ -14,14 +14,16 @@ export default function Shelter({ content }) {
 
   return (
     <>
-      {centers.length > 0 ?
+      {centers.totalResults > 0 ?
       <Flex direction={'column'} as={Container} maxW="7xl" mt={5} p={4}>
         <Text p={4} mb={2} w={"100%"} fontSize={{ base: "15px", md: "2xl", lg: "17px" }} textAlign={"center"}>{content}</Text>
         <Flex direction={{ base: 'row', sm: 'column', md: 'column', lg: 'row'}} alignItems={'center'} justifyContent={'space-around'} w={'100%'} mt={5}>
-          {centers.map(shelter => (
-              <WrapItem cursor={'pointer'} key={shelter.id}>
-                <Image p={4} w={180} alt={shelter.name} src={shelter.picture ? `${process.env.NEXT_PUBLIC_HOST}${shelter.picture.url.substring(1,1000)}` : './dog.svg' } />
-              </WrapItem>  
+          {centers.results.map(shelter => (
+              <NextLink href={`/centros/${shelter.id}`} passHref>
+                <WrapItem cursor={'pointer'} key={shelter.id}>
+                  <Image p={4} w={180} alt={shelter.name} src={shelter.picture ? `${process.env.NEXT_PUBLIC_HOST}${shelter.picture.url.substring(1,1000)}` : './dog.svg' } />
+                </WrapItem>
+              </NextLink>
             )
           )}
         </Flex>
