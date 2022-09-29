a58c8060ef258f77905066c291b9fac3ea5bb11a
diloes <diloes@hotmail.com>
2022-09-29 18:34:36 +0200

    fix: change file extension jsx to js & mocks modif

--- good-paws-web/src/mocks/mocks.js
+++ good-paws-web/src/mocks/mocks.js
@@ -1,27 +1,27 @@
-export const shelters = [
-    {
-      id: "1",
-      name: "Shelter",
-      avatar: './shelter.svg'
-    },
-    {
-      id: "2",
-      name: "Shelter",
-      avatar: './shelter.svg'
-    },
-    {
-      id: "3",
-      name: "Shelter",
-      avatar: './shelter.svg'
-    },
-    {
-      id: "4",
-      name: "Shelter",
-      avatar: './shelter.svg'
-    },
-    {
-      id: "5",
-      name: "Shelter",
-      avatar: './shelter.svg'
-    },
-];
+export const products = [
+  {
+    id: '1',
+    name: 'Shelter',
+    avatar: './shelter.svg',
+  },
+  {
+    id: '2',
+    name: 'Shelter',
+    avatar: './shelter.svg',
+  },
+  {
+    id: '3',
+    name: 'Shelter',
+    avatar: './shelter.svg',
+  },
+  {
+    id: '4',
+    name: 'Shelter',
+    avatar: './shelter.svg',
+  },
+  {
+    id: '5',
+    name: 'Shelter',
+    avatar: './shelter.svg',
+  },
+]
---
+++ good-paws-web/src/pages/pet/[id].js
@@ -0,0 +1,45 @@
+import { BackArrow } from '../../components/BackArrow'
+import { PetDetails } from '../../components/PetDetails'
+import { PetInfoSheet } from '../../components/PetInfoSheet'
+import { PetPictures } from '../../components/PetPictures'
+import { ProtectorWidget } from '../../components/ProtectorWidget'
+import { SimilarPets } from '../../components/SimilarPets'
+import useScreenSize from '../../hooks/useScreenSize'
+
+import { Grid, GridItem1, GridItem2, GridItem3 } from './styles'
+
+const PetOverview = ({ data }) => {
+  const { width } = useScreenSize()
+
+  return (
+    <Grid>
+      <GridItem1>
+        {width >= 785 && <BackArrow text="Volver al listado" link="/petlist" />}
+        {width >= 785 && <PetPictures data={data} />}
+      </GridItem1>
+      <GridItem2>
+        <PetDetails data={data} />
+      </GridItem2>
+      <GridItem3>
+        {width >= 785 && <PetInfoSheet data={data} />}
+        <ProtectorWidget />
+        <SimilarPets />
+      </GridItem3>
+    </Grid>
+  )
+}
+
+export const getServerSideProps = async context => {
+  const res = await fetch(
+    `https://63058cf4697408f7edc71966.mockapi.io/pets/${context.query.id}`
+  )
+  const data = await res.json()
+
+  return {
+    props: {
+      data,
+    },
+  }
+}
+
+export default PetOverview
