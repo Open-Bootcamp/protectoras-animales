50e9a65b7e4e2fe26ff05d14828c1ba2b605aa2c
diloes <diloes@hotmail.com>
2022-09-29 18:16:05 +0200

    fix: renamed some files due problems in render

--- good-paws-web/src/components/FindNewPartner/index.jsx
+++ good-paws-web/src/components/FindNewPartner/index.jsx
@@ -1,96 +1,108 @@
-import React, { useContext, useEffect } from "react";
-import { useClickable } from "@chakra-ui/clickable";
+import { useClickable } from '@chakra-ui/clickable'
 import {
-  chakra,
-  Text,
   Box,
-  Divider,
+  chakra,
   Container,
+  Divider,
   Flex,
   Stack,
+  Text,
   useColorModeValue,
-} from "@chakra-ui/react";
-import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
-import HeadBox from "./Components/HeadBox";
-import CardsContainer from "../CardsContainer/CardsContainer";
-import Sidebar from "./Components/Sidebar";
-import { MainContext } from "../../context/maincontext";
-import { useDispatch, useSelector } from "react-redux";
-import { getAnimals } from '../../store/animalsSlice';
-import { usePagination } from "react-use-pagination";
+} from '@chakra-ui/react'
+import React, { useContext, useEffect } from 'react'
+import { FaArrowLeft, FaArrowRight } from 'react-icons/fa'
+import { useDispatch, useSelector } from 'react-redux'
+import { usePagination } from 'react-use-pagination'
+import { MainContext } from '../../context/maincontext'
+import { getAnimals } from '../../store/animalsSlice'
+import CardsContainer from '../CardsContainer/CardsContainer'
+import HeadBox from './components/HeadBox'
+import Sidebar from './components/Sidebar'
 
-const Clickable = (props) => {
-  const clickable = useClickable(props);
+const Clickable = props => {
+  const clickable = useClickable(props)
   return (
     <chakra.button
       border={`1px solid gray`}
-      rounded={"md"}
+      rounded={'md'}
       w={8}
       h={8}
-      cursor={"pointer"}
+      cursor={'pointer'}
       onClick
-      display={"inline-flex"}
-      alignItems={"center"}
-      justifyContent={"center"}
-      transition={"background 0.3s ease"}
-      _hover={{ bg: "primary", color: "white" }}
+      display={'inline-flex'}
+      alignItems={'center'}
+      justifyContent={'center'}
+      transition={'background 0.3s ease'}
+      _hover={{ bg: 'primary', color: 'white' }}
       {...clickable}
     />
-  );
-};
+  )
+}
 
 const FindNewPartner = () => {
-  const { filters, userLocation } = useContext(MainContext);
-  const dispatch = useDispatch();
-  const data = useSelector((state) => state.animals);
-  const { currentPage, totalPages, setNextPage, setPreviousPage, nextEnabled, previousEnabled } = usePagination({ totalItems: data.animals.totalResults, initialPageSize: 8 });
+  const { filters, userLocation } = useContext(MainContext)
+  const dispatch = useDispatch()
+  const data = useSelector(state => state.animals)
+  const {
+    currentPage,
+    totalPages,
+    setNextPage,
+    setPreviousPage,
+    nextEnabled,
+    previousEnabled,
+  } = usePagination({
+    totalItems: data.animals.totalResults,
+    initialPageSize: 8,
+  })
 
   useEffect(() => {
-    dispatch(getAnimals({ filters, initRegs: 8, currentPage, userLocation  }));
-  }, [filters, currentPage]);
+    dispatch(getAnimals({ filters, initRegs: 8, currentPage, userLocation }))
+  }, [filters, currentPage])
 
   return (
     <Stack p={10}>
-      <Flex direction={"column"}>
+      <Flex direction={'column'}>
         <HeadBox />
-        <Flex direction={{ base: 'column', md: 'row'}}>
+        <Flex direction={{ base: 'column', md: 'row' }}>
           <Sidebar />
           <Flex w={'full'} direction={'column'}>
-            <CardsContainer data={data.animals.results && data.animals.results} />
-            <Box color={useColorModeValue("gray5", "gray2")}>
+            <CardsContainer
+              data={data.animals.results && data.animals.results}
+            />
+            <Box color={useColorModeValue('gray5', 'gray2')}>
               <Divider orientation="horizontal" />
               <Container
                 as={Stack}
-                maxW={"6xl"}
+                maxW={'6xl'}
                 py={4}
-                direction={{ base: "column", md: "row" }}
+                direction={{ base: 'column', md: 'row' }}
                 spacing={4}
-                justify={{ base: "center", md: "space-between" }}
-                align={{ base: "center", md: "center" }}
+                justify={{ base: 'center', md: 'space-between' }}
+                align={{ base: 'center', md: 'center' }}
               >
                 <Flex
-                  w={"100%"}
-                  justifyContent={"space-between"}
-                  direction={"row"}
+                  w={'100%'}
+                  justifyContent={'space-between'}
+                  direction={'row'}
                   spacing={6}
-                  alignItems={"center"}
+                  alignItems={'center'}
                 >
                   <Clickable
                     as="button"
                     onClick={setPreviousPage}
-                    _active={{ bg: "primarylight", color: "white" }}
+                    _active={{ bg: 'primarylight', color: 'white' }}
                     _disabled={!previousEnabled}
                   >
                     <FaArrowLeft />
                   </Clickable>
                   <Text>
-                    Página {totalPages > 0 ? currentPage + 1 : currentPage} de{" "}
+                    Página {totalPages > 0 ? currentPage + 1 : currentPage} de{' '}
                     {totalPages}
                   </Text>
                   <Clickable
                     as="button"
                     onClick={setNextPage}
-                    _active={{ bg: "primarylight", color: "white" }}
+                    _active={{ bg: 'primarylight', color: 'white' }}
                     _disabled={!nextEnabled}
                   >
                     <FaArrowRight />
@@ -102,7 +114,7 @@ const FindNewPartner = () => {
         </Flex>
       </Flex>
     </Stack>
-  );
-};
+  )
+}
 
-export default FindNewPartner;
+export default FindNewPartner
--- good-paws-web/src/pages/api/pets.js
+++ good-paws-web/src/pages/api/pets.js
@@ -1,5 +1,5 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import { products } from "../../Mocks/mocks"
+import { products } from '../../mocks/mocks'
 
 export default function handler(req, res) {
   res.status(200).json(products)
