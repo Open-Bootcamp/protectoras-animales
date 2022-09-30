8d0380196c625aa113ce939aa89ff0eadc27ff56
AMDevelopOB <amdevelop@open-bootcamp.com>
2022-09-30 13:50:40 +0200

    refactor: New branch for Admin Panel

--- good-paws-web/src/pages/api/pets.js
+++
@@ -1,6 +0,0 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import { products } from '../../mocks/mocks'
-
-export default function handler(req, res) {
-  res.status(200).json(products)
-}
